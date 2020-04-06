8d3bdef01952ecb428ac2dfb3710ed3e59f703c5
Dyrits <dyrits@outlook.fr>
2020-04-06 20:59:13 +0200

    Minor update.

--- Sudoku Solver/script.js
+++ Sudoku Solver/script.js
@@ -1,22 +1,34 @@
-let sudokuGrid = document.querySelector("#grid")
-let cells = document.querySelectorAll(".cell");
-let button = document.querySelector("#solve")
+const gridBox = document.querySelector("#grid")
+const mainButton = document.querySelector("#mainButton")
+const commands = document.querySelector("#commands")
+const resetButton = document.createElement("button");
+resetButton.innerHTML = "Reset!";
+resetButton.classList = "button";
+let sudokuGrid = [[], [], [], [], [], [], [], [], []]
 
+
+// Function creating the grid as an array and displaying it:
 function generateGrid() {
   for (iteration = 0; iteration < 81; iteration++) {
-    var sudokuCell = document.createElement("input");
-    sudokuCell.type = "number"
-    sudokuCell.classList = "cell"
-    sudokuCell.id = "cell" + iteration
-    sudokuCell.min = "1"
-    sudokuCell.max = "9"
-    sudokuGrid.appendChild(sudokuCell);
+    indexRow = Math.floor(iteration / 9)
+    indexColumn = iteration % 9
+    sudokuGrid[indexRow][indexColumn] = document.createElement("input");
+    sudokuGrid[indexRow][indexColumn].type = "number"
+    sudokuGrid[indexRow][indexColumn].classList = "cell"
+    sudokuGrid[indexRow][indexColumn].id = "cell" + iteration
+    sudokuGrid[indexRow][indexColumn].min = "1"
+    sudokuGrid[indexRow][indexColumn].max = "9"
+    gridBox.appendChild(sudokuGrid[indexRow][indexColumn]);
   }
 }
 
-button.addEventListener("click", function() {
-  if (button.innerHTML === "Generate!") {
+function mainButtonClick() {
+  if (mainButton.innerHTML === "Generate!") {
     generateGrid();
-    button.innerHTML = "Submit!"}
-});
+    mainButton.innerHTML = "Submit!";
+    commands.appendChild(resetButton);
+  }
+}
 
+mainButton.addEventListener("click", mainButtonClick);
+// resetButton.addEventListener("click", resetButtonClick);
